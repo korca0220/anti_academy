8aafa3f9a13d63c11587672e03f00c7c8aa2dd99
ParkJuneWoo <krca0220@gmail.com>
2026-02-08 18:06:41 +0900

    add supabase init

--- bridge/lib/main.dart
+++ bridge/lib/main.dart
@@ -1,16 +1,22 @@
-import 'package:bridge/app/router/app_router.dart';
-import 'package:bridge/app/theme/app_theme.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 // TODO: 1. Import AppRouter
 // import 'package:bridge/app/router/app_router.dart';
 // TODO: 2. Import AppTheme
 // import 'package:bridge/app/theme/app_theme.dart';
 
+import 'package:bridge/app/constants/supabase_config.dart';
+import 'package:bridge/app/router/app_router.dart';
+import 'package:bridge/app/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/Supabase_flutter.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO: Initialize Supabase (Next Step!)
+  await Supabase.initialize(
+    url: SupabaseConfig.url,
+    anonKey: SupabaseConfig.anonKey,
+  );
 
   // TODO: 3. Wrap the app with ProviderScope
   runApp(
