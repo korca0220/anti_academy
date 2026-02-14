1bfbe046d1739d78c7e328e21fc832298f99a00d
ParkJuneWoo <krca0220@gmail.com>
2026-02-14 15:06:26 +0900

    update profile widget

--- bridge/lib/features/feed/presentation/screens/post_detail_screen.dart
+++ bridge/lib/features/feed/presentation/screens/post_detail_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../profile/presentation/providers/profile_providers.dart';
 import '../../domain/entities/post.dart';
 
-class PostDetailScreen extends StatelessWidget {
+class PostDetailScreen extends ConsumerWidget {
   const PostDetailScreen({super.key, required this.post});
 
   final Post post;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final dateStr = post.createdAt.toLocal().toString().split('.')[0];
+    final profile = ref.watch(profileFutureProvider(post.authorId));
 
     return Scaffold(
       appBar: AppBar(
@@ -31,19 +34,32 @@ class PostDetailScreen extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  CircleAvatar(
-                    child: Icon(Icons.person),
+                  profile.when(
+                    data: (profile) => CircleAvatar(
+                      backgroundImage: profile?.avatarUrl != null //
+                          ? NetworkImage(profile?.avatarUrl ?? '')
+                          : null,
+                      child: profile?.avatarUrl != null //
+                          ? null
+                          : Icon(Icons.person),
+                    ),
+                    error: (_, __) => SizedBox.shrink(),
+                    loading: () => SizedBox.shrink(),
                   ),
                   SizedBox(width: 12),
                   Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'User Id : ${post.authorId}',
-                          style: Theme.of(context).textTheme.labelLarge,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
+                        profile.when(
+                          data: (profile) => Text(
+                            'By ${profile?.nickname ?? 'Unknown'}',
+                            style: Theme.of(context).textTheme.labelLarge,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                          ),
+                          error: (_, __) => SizedBox.shrink(),
+                          loading: () => SizedBox.shrink(),
                         ),
                         Text(
                           dateStr,
--- bridge/lib/features/feed/presentation/widgets/feed_item.dart
+++ bridge/lib/features/feed/presentation/widgets/feed_item.dart
@@ -1,8 +1,11 @@
 import 'package:bridge/features/feed/domain/entities/post.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class FeedItem extends StatelessWidget {
+import '../../../profile/presentation/providers/profile_providers.dart';
+
+class FeedItem extends ConsumerWidget {
   const FeedItem({
     super.key,
     required this.post,
@@ -11,13 +14,35 @@ class FeedItem extends StatelessWidget {
   final Post post;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final profile = ref.watch(profileFutureProvider(post.authorId));
+
     return ListTile(
       onTap: () {
         context.push('/post-detail', extra: post);
       },
-      title: Text(post.title),
-      subtitle: Text(post.content),
+      title: Text(
+        post.title,
+        style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(post.content),
+          SizedBox(height: 4),
+          profile.when(
+            error: (_, __) => SizedBox.shrink(),
+            loading: () => SizedBox.shrink(),
+            data: (profile) => Text(
+              'By ${profile?.nickname ?? 'Unknown'}',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ),
+        ],
+      ),
       leading: switch (post.type) {
         PostType.request => Badge(label: Text('Request'), backgroundColor: Colors.blue),
         PostType.offer => Badge(label: Text('Offer'), backgroundColor: Colors.green),
