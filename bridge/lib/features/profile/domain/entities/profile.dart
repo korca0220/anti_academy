843c851dad5e126de62e41e31516869e53630ff5
ParkJuneWoo <krca0220@gmail.com>
2026-02-16 02:08:35 +0900

    update profile entity

--- bridge/lib/features/profile/domain/entities/profile.dart
+++ bridge/lib/features/profile/domain/entities/profile.dart
@@ -8,9 +8,9 @@ class Profile with _$Profile {
   const factory Profile({
     required String id,
     required String nickname,
-    @JsonKey(name: 'avatar_url') String? avatarUrl,
+    String? avatarUrl,
     String? bio,
-    @JsonKey(name: 'created_at') required DateTime createdAt,
+    required DateTime createdAt,
   }) = _Profile;
 
   factory Profile.fromJson(Map<String, dynamic> json) => _$ProfileFromJson(json);
